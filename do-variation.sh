f742c71dcb52565d064aad2083126a973ded36e8
vijayananth.s <ananth.vijay@gmail.com>
2014-10-24 10:30:05 -0400

    do-variation for executing raceDriver writtern
    a simple shell script for executing raceDriver code and record the time
    it takes to run in the kernel

---
+++ do-variation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -v
+echo xxxx
+time ./raceDriver- 1
+echo xxxx
+time ./raceDriver- 1
+echo xxxx
+time ./raceDriver- 1
+echo xxxx
+time ./raceDriver- 2
+echo xxxx
+time ./raceDriver- 2 
+echo xxxx
+time ./raceDriver- 2 
+echo xxxx
+time ./raceDriver- 10
+echo xxxx
+time ./raceDriver- 10
+echo xxxx
+time ./raceDriver- 10
+echo xxxx
+cat /proc/cpuinfo 
+
+
